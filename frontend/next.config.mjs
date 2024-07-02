d229b7ea36af2c0c0713af45b62630dea863fc11
SimonSun22 <84614775+SimonSun22@users.noreply.github.com>
2024-07-01 19:01:47 -0700

    fix(frontend): Updated next.config to solve sdk import issue & generate and verify proof feature still WIP

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    transpilePackages: [
+        '@galxe-identity-protocol/sdk',
+      ],
     compiler: {
         styledComponents: true,
     },
